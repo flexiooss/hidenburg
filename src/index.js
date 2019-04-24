c4f2d059bad6d64d87586ba8ee5a435e36780689
Bruno FL <follet-bruno@hotmail.fr>
2019-04-24 14:46:27 +0200

    Item + export Item

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './generated/io/package'
 
 export {ComponentSelect} from './component/ComponentSelect'
+export {ComponentSelectConfig} from './component/ComponentSelectConfig'
 export {ProxyStoreInterface} from './component/ProxyStoreInterface'
 export {ViewItemBuilderInterface} from './view/ViewItemBuilderInterface'
+export {Item} from './generated/io/flexio/component_select/types/Item'
