968d4c964fb5c6d377be12d9d0002954c12d749f
Bruno FL <follet-bruno@hotmail.fr>
2019-04-24 14:36:09 +0200

    Fix exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export ComponentSelect from './src/component/ComponentSelect'
 export {ComponentSelect} from './src/component/ComponentSelect'
+export {ComponentSelectConfig} from './src/component/ComponentSelectConfig'
 export {ProxyStoreInterface} from './src/component/ProxyStoreInterface'
 export {ViewItemBuilderInterface} from './src/view/ViewItemBuilderInterface'
