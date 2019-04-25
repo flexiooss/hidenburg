fb6c15f24c569aeb5078a070d776e75b07fa4bd8
Bruno FL <follet-bruno@hotmail.fr>
2019-04-25 10:04:01 +0200

    ItemListBuilder

---
+++ src/component/ItemListBuilder.js
@@ -0,0 +1,27 @@
+import {ItemList} from "./ItemList";
+import {ItemBuilder} from "../generated/io/flexio/component_select/types/Item";
+
+export class ItemListBuilder {
+  constructor() {
+    this.__itemList = new ItemList()
+  }
+
+  /**
+   *
+   * @param {string} value
+   * @param {string} label
+   * @param {boolean} [visible=true]
+   * @param {boolean} [selected=false]
+   * @param {boolean} [disabled=false]
+   * @returns {ItemListBuilder}
+   */
+  addItem(value, label, visible = true, selected = false, disabled = false) {
+    this.__itemList.push(new ItemBuilder().value(value).label(label)
+      .seleted(selected).disabled(disabled).build())
+    return this
+  }
+
+  build() {
+    return this.__itemList
+  }
+}
